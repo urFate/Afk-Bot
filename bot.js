c45157259122035dbeaf1c06dfa55f9d05d5c1e6
ArnyFour <43936310+Arny4@users.noreply.github.com>
2021-01-07 20:22:25 +0300

    Version 1.0.0

---
+++ bot.js
@@ -0,0 +1,89 @@
+const mineflayer = require('mineflayer')
+const Movements = require('mineflayer-pathfinder').Movements
+const pathfinder = require('mineflayer-pathfinder').pathfinder
+const { GoalBlock} = require('mineflayer-pathfinder').goals
+
+const config = require('./settings.json');
+
+console.log(" \n ")
+console.log("░█████╗░███████╗██╗░░██╗  ██████╗░░█████╗░████████╗  ██╗░░░██╗░░███╗░░░░░░█████╗░")
+console.log("██╔══██╗██╔════╝██║░██╔╝  ██╔══██╗██╔══██╗╚══██╔══╝  ██║░░░██║░████║░░░░░██╔══██╗")
+console.log("███████║█████╗░░█████═╝░  ██████╦╝██║░░██║░░░██║░░░  ╚██╗░██╔╝██╔██║░░░░░██║░░██║")
+console.log("██╔══██║██╔══╝░░██╔═██╗░  ██╔══██╗██║░░██║░░░██║░░░  ░╚████╔╝░╚═╝██║░░░░░██║░░██║")
+console.log("██║░░██║██║░░░░░██║░╚██╗  ██████╦╝╚█████╔╝░░░██║░░░  ░░╚██╔╝░░███████╗██╗╚█████╔╝")
+console.log("╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝  ╚═════╝░░╚════╝░░░░╚═╝░░░  ░░░╚═╝░░░╚══════╝╚═╝░╚════╝░")
+console.log(" \n ")
+console.log("")
+
+function createBot () {
+  const bot = mineflayer.createBot({
+      username: config['bot-account']['username'],
+      password: config['bot-account']['password'],
+      host: config.server.ip,
+      port: config.server.port,
+      version: config.server.version
+  })
+
+  bot.loadPlugin(pathfinder)
+  const mcData = require('minecraft-data')(bot.version)
+  const defaultMove = new Movements(bot, mcData)
+  bot.settings.colorsEnabled = false
+
+  
+
+  bot.once("spawn", function(){
+      console.log("\x1b[33m[BotLog] Bot joined to the server", '\x1b[0m')
+
+      if(config.utils['auto-auth'].enabled){
+          var password = config.utils['auto-auth'].password
+          setTimeout(function() {
+              bot.chat(`/register ${password} ${password}`)
+              bot.chat(`/login ${password}`)
+          }, 500);
+      }
+
+      const pos = config.position
+
+      if (config.position.enabled){
+          console.log(`\x1b[32m[BotLog] Starting moving to target location (${pos.x}, ${pos.y}, ${pos.z})\x1b[0m`)
+          bot.pathfinder.setMovements(defaultMove)
+          bot.pathfinder.setGoal(new GoalBlock(pos.x, pos.y, pos.z))
+      }
+      
+      if(config.utils['anti-afk'].enabled){
+        bot.setControlState('jump', true)
+        if(config.utils['anti-afk'].sneak){
+            bot.setControlState('sneak', true)
+        }
+      }
+  })
+
+  bot.on("chat", function(username, message){
+      if(config.utils['chat-log']){
+
+          console.log(`[ChatLog] ${username} : ${message}`)
+      }
+  })
+
+  bot.on("goal_reached", function(){
+      console.log(`\x1b[32m[BotLog] Bot arrived to target location. ${bot.entity.position}\x1b[0m`)
+  })
+
+  bot.on("death", function(){
+      console.log(`\x1b[33m[BotLog] Bot has been died and was respawned ${bot.entity.position}`, '\x1b[0m')
+  })
+
+  if(config.utils['auto-reconnect']){
+      bot.on('end', function(){
+        createBot()
+      })
+  }
+
+  
+
+  bot.on('kicked', (reason) => console.log('\x1b[33m',`[BotLog] Bot was kicked from the server. Reason: \n${reason}`, '\x1b[0m'))
+  bot.on('error', err => console.log(`\x1b[31m[ERROR] ${err.message}`, '\x1b[0m'))
+
+}
+
+createBot()
