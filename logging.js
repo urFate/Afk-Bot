2319946678120b3949ececfb22ef95b0e8a1fabb
urFate <georgiylakidon@gmail.com>
2023-01-05 17:53:01 +0300

    Don't save log to file

--- logging.js
+++ logging.js
@@ -2,16 +2,10 @@ const log4js = require("log4js");
 log4js.configure({
     appenders: {
         console: { type: "console" },
-        file: {
-            type: 'dateFile',
-            filename: 'bot.log',
-            numBackups: 3,
-            pattern: 'yyyy-MM-dd',
-        }
     },
     categories: {
         default: {
-            appenders: ["file", "console"],
+            appenders: ["console"],
             level: "info"
         }
     },
